2844eb831ad362bf900e47099c58844047e8ad53
杉山希望 <rioc.sugiyama@gmail.com>
2018-05-16 23:19:30 +0900

    add GetUsers function

--- server/model/user.go
+++ server/model/user.go
@@ -2,35 +2,34 @@ package model
 
 import (
 	"fmt"
-
 	"gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
-	ID                   int      `bson:"_id"`
-	DisplayName          string   `bson:"display_name"`
-	FirstName            string   `bson:"first_name"`
-	LastName             string   `bson:"last_name"`
-	FirstNameEnglish     string   `bson:"first_name_english"`
-	LastNameEnglish      string   `bson:"last_name_english"`
-	Birthday             string   `bson:"birthday"`
-	Gender               string   `bson:"gender"`
-	Email                string   `bson:"email"`
-	EmailForShared       string   `bson:"email_for_shared"`
-	CountryPrefecture    string   `bson:"country_prefecture"`
-	PhoneNumber          string   `bson:"phone_number"`
-	SlackID              string   `bson:"slack_id"`
-	GithubID             string   `bson:"github_id"`
-	License              []string `bson:"license"`
+	ID                   bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	DisplayName          string        `json:"display_name" bson:"display_name"`
+	FirstName            string        `json:"first_name" bson:"first_name"`
+	LastName             string        `json:"last_name" bson:"last_name"`
+	FirstNameEnglish     string        `json:"first_name_english" bson:"first_name_english"`
+	LastNameEnglish      string        `json:"last_name_english" bson:"last_name_english"`
+	Birthday             string        `json:"birthday" bson:"birthday"`
+	Gender               string        `json:"gender" bson:"gender"`
+	Email                string        `json:"email" bson:"email"`
+	EmailForShared       string        `json:"email_for_shared" bson:"email_for_shared"`
+	CountryPrefecture    string        `json:"country_prefecture" bson:"country_prefecture"`
+	PhoneNumber          string        `json:"phone_number" bson:"phone_number"`
+	SlackID              string        `json:"slack_id" bson:"slack_id"`
+	GithubID             string        `json:"github_id" bson:"github_id"`
+	License              []string      `json:"license" bson:"license"`
 	SubscriptionReligion struct {
-		ID string `bson:"_id"`
-	} `bson:"subscription_religion"`
+		ID string `json:"_id" bson:"_id,omitempty"`
+	} `json:"subscription_religion" bson:"subscription_religion"`
 }
 
-type Users []User
-
-func GetUsers() (Users, error) {
+func GetUsers() ([]User, error) {
 	var session = GetSession("mongodb")
+	defer session.Close()
+
 	db := session.DB("test")
 
 	var users []User
@@ -44,14 +43,16 @@ func CreateUser(user User) error {
 	return nil
 }
 
-func GetUser(id int) (User, error) {
+func GetUser(id string) (User, error) {
 	var session = GetSession("mongodb")
+	defer session.Close()
+
 	db := session.DB("test")
 
 	user := User{}
-	db.C("users").Find(bson.M{"_id": id}).One(&user)
-	fmt.Println("Results: ", user)
-	
+	db.C("users").Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&user)
+	fmt.Println("Results: ", user, id)
+
 	return user, nil
 }
 
